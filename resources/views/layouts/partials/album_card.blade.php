10a642c896e0ec318539ffb3f70b1f0292aa2b0d
EliaBuratti <elia.buratti.dev@gmail.com>
2023-10-27 13:55:21 +0200

    fix class name

--- resources/views/layouts/partials/album_card.blade.php
+++ resources/views/layouts/partials/album_card.blade.php
@@ -3,7 +3,7 @@
 @section ('content_main')
 <div class=" eb_cards p-5">
     <div class="container d-flex flex-wrap gap-5 justify-content-center">
-        <h3>current series</h3>
+        <h3 class="eb_series">current series</h3>
         @foreach(Config::get('db_comic.comic_details') as $thumb)
 
         <div class="eb_card">
